bad6850c0abfe20df86323793087f899d91e4e9c
CHINAZA BELOLISA <chinazabelolisa@gmail.com>
2022-09-21 13:29:28 -0500

    first commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("<series>", views.series, name="series"),
+    path("<series>/<article>", views.article, name="article"),
+    path("", include("allauth.urls"))
+ 
+]
